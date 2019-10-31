d632ff2bff338e0335e8f31a7a9a6c5058395644
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-31 13:56:00 +0800

    Add api_getUserID

--- app.py
+++ app.py
@@ -17,7 +17,10 @@ import requests
 
 def api_getUserID(username=None):
     api_headers = {'identity': 'Group2', 'token': '2c55f846-e723-4564-b93c-35630007dd7a'}
-    api_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan'
+    if username is None:
+        api_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan'
+    else:
+        api_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/' + username
     response = requests.get(api_url, headers=api_headers)
 
     if response.status_code == 200:
