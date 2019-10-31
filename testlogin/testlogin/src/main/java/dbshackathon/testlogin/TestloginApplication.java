b67c5f546fce815adffc63c41728f6a0353669ac
Amartur Rahim <amarturrahim1795@gmail.com>
2019-10-31 10:16:44 +0800

    Initial Commit: Spring Boot Template

---
+++ testlogin/testlogin/src/main/java/dbshackathon/testlogin/TestloginApplication.java
@@ -0,0 +1,13 @@
+package dbshackathon.testlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestloginApplication.class, args);
+	}
+
+}
