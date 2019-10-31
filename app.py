940cd8090ad2a9d841cafa298b06f9dfa70bc3c3
veralygit <57183851+veralygit@users.noreply.github.com>
2019-10-31 14:34:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -7,29 +7,24 @@ from flask import (
     session,
     request,
     logging,
-    Response,
-    Request
 )
-
-import webapi
 
 app = Flask(__name__)
 
 # Index
-@app.route("/")
+@app.route("/home")
 def index():
     return render_template("home.html")
 
+# Dashboard
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 # About
 @app.route("/about")
 def about():
     return render_template("about.html")
-
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
 
 
 if __name__ == "__main__":
