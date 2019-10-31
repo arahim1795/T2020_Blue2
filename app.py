e1080fa4d4ff54d8405ddd109ff8ee3ea585268b
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-31 14:29:32 +0800

    Add web api functions: api_getCustomerDetails api_getListOfDepositAccounts api_getTransactionDetails api_getAccountBalance api_getListOfCreditAccounts

--- app.py
+++ app.py
@@ -11,26 +11,9 @@ from flask import (
     Request
 )
 
-import json
-import requests
-
-
-def api_getUserID(username=None):
-    api_headers = {'identity': 'Group2', 'token': '2c55f846-e723-4564-b93c-35630007dd7a'}
-    if username is None:
-        api_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan'
-    else:
-        api_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/' + username
-    response = requests.get(api_url, headers=api_headers)
-
-    if response.status_code == 200:
-        data = json.loads(response.content.decode('utf-8'))
-        return data["customerId"]
-    else:
-        return None
+import webapi
 
 app = Flask(__name__)
-
 
 # Index
 @app.route("/")
