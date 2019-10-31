96c2c126e0215f200fe31a8427713545ca3a6ab1
Frank Nguyen <frank.nguyen.vd@gmail.com>
2019-10-31 13:54:26 +0800

    Add api_getUserID

--- app.py
+++ app.py
@@ -7,9 +7,27 @@ from flask import (
     session,
     request,
     logging,
+    Response,
+    Request
 )
 
+import json
+import requests
+
+
+def api_getUserID(username=None):
+    api_headers = {'identity': 'Group2', 'token': '2c55f846-e723-4564-b93c-35630007dd7a'}
+    api_url = 'http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com/customers/marytan'
+    response = requests.get(api_url, headers=api_headers)
+
+    if response.status_code == 200:
+        data = json.loads(response.content.decode('utf-8'))
+        return data["customerId"]
+    else:
+        return None
+
 app = Flask(__name__)
+
 
 # Index
 @app.route("/")
@@ -21,6 +39,11 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
